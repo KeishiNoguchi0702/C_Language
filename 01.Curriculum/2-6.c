1b9015e2f99b430c86e94dea2a00e2690db56807
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-12-20 11:33:04 +0900

    Normal

---
+++ 01.Curriculum/2-6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+  const int TAX = 108;
+  int fax = 50000;
+  printf("50,000円から40,000円に値下げします\n");
+  TAX = 40000;
+  printf("FAXの新価格（税込み）：%d円\n", fax * TAX / 100);
+  return 0;
+}
+
+// constを使用して変数が書き換えられないさまを確認する
