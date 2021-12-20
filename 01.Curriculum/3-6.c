e58b74ffc4992361335a0d6e2aabf0a8f2dd1929
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-12-20 13:55:06 +0900

    Normal

---
+++ 01.Curriculum/3-6.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i = 3.2;
+  printf("%d\n", i);
+  return 0;
+}
---
+++ 01.Curriculum/3-7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+  int age = (int)3.2;
+  printf("%d\n", age);
+  return 0;
+}
+
+// 明示的に型変換を行うサンプル（当然、コンパイル時の警告文は表示されない）
