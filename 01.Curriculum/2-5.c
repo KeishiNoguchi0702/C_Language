4bc417a9f6691c3d2f7efecd16182c2bfa2a477a
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-12-20 11:29:52 +0900

    Normal

--- 01.Curriculum/2-5.c
+++ 01.Curriculum/2-5.c
@@ -7,7 +7,7 @@ int main(void)
   printf("50,000円から40,000円に値下げします\n");
   tax = 40000;
   printf("FAXの新価格（税込み）：%d円\n", fax * tax / 100);
-  return 0
+  return 0;
 }
 
 // 固定したい変数が書き換えられてしまうと何が起こるか理解するための節
