3070328b73226201f64922cfa7524995074317e0
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-12-20 11:44:51 +0900

    Normal

--- 01.Curriculum/2-6.c
+++ 01.Curriculum/2-6.c
@@ -5,6 +5,7 @@ int main(void)
   const int TAX = 108;
   int fax = 50000;
   printf("50,000円から40,000円に値下げします\n");
+  TAX = 40000;                                            //ここでコンパイルエラーが発生するので、間違いにきづける
   printf("FAXの新価格（税込み）：%d円\n", fax * TAX / 100);
   return 0;
 }
