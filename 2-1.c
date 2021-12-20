3bb3dfa6c7548a9b87ce71369bf6865f09c19017
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-12-20 11:05:15 +0900

    Normal

--- 2-1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-  int age;
-  age = 30;
-  printf("%d¥n", age);
-  return 0;
-}
--- 2-2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-  char c = 100;
-  char d = c + 100; // make overflow
-  printf("%d¥n", d);
-  return 0;
-}
