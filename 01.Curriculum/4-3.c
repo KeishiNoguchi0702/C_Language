1eb0bc725cef0b03c854477378c1c8b139204c94
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-12-21 18:27:46 +0900

    Normal

---
+++ 01.Curriculum/4-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+  bool tenki = true;
+  if (tenki == true) {
+    printf("洗濯しますね\nお散歩に行きますね\n");
+  } else
+    printf("映画を観ますね\n");
+    return 0;
+}
