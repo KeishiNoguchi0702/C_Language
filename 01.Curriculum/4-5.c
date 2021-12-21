8e553403782174e1c9922958c0248e73384da4fb
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-12-21 18:45:42 +0900

    Normal

---
+++ 01.Curriculum/4-5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef char String[1024];
+
+int main(void)
+{
+  String answerNo;
+  printf("カッコよくて最高な、C言語男子の名前は？\n");
+  printf("1: Keishi 2: Keiji 3:Keipi 4:Kenji\n");
+  scanf("%s", answerNo);
+  int n = atoi(answerNo);
+
+  if (n == 1) {
+    printf("大正解！見る目あるね！\n");
+  } else {
+    printf("残念。\n");
+  }
+  return 0;
+}
