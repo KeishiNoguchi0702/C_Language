d1fc564f138a0bff35c533d3dd00ff8d8103861e
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-12-22 15:47:24 +0900

    Normal

--- 01.Curriculum/5-5.c
+++ 01.Curriculum/5-5.c
@@ -2,9 +2,11 @@
 
 int main(void)
 {
-  for (int i = 1; i <= 10; i ++) {
-    printf("He is the one.\n");
+  for (int i = 1; i <= 6; i ++) {
+    printf("The Matrix has been reloaded.\n");
     printf("%d\n", i);
   }
+  printf("The Matrix has been Revolutions.\n");
+  printf("%d\n", 7);
   return 0;
 }
