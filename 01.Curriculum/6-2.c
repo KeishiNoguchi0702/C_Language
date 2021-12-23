9dcaf583a793784c3797cb5c80ae35469848f3e0
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2021-12-23 10:55:01 +0900

    Normal

--- 01.Curriculum/6-2.c
+++ 01.Curriculum/6-2.c
@@ -4,16 +4,22 @@ typedef char String[1024];
 
 int main(void)
 {
-  struct MONSTER {
+  typedef struct {
     String name;
     int hp;
     int attack;
-  };
+  } Monster;
 
-  struct MONSTER seiryu;
-  struct MONSTER suzaku;
-  struct MONSTER byakko;
-  struct MONSTER genbu;
+  Monster seiryu = { "青龍", 100, 30 };
+  Monster suzaku = { "朱雀", 120, 20 };
+  Monster byakko = { "白虎", 110, 30 };
+  Monster genbu =  { "玄武",  90, 40 };
+
+  const String TEMPLATE = "%s : HP=%3d 攻撃力=%2d\n";
+  printf(TEMPLATE, seiryu.name, seiryu.hp, seiryu.attack);
+  printf(TEMPLATE, suzaku.name, suzaku.hp, suzaku.attack);
+  printf(TEMPLATE, byakko.name, byakko.hp, byakko.attack);
+  printf(TEMPLATE, genbu.name, genbu.hp, genbu.attack);
 
   return 0;
 }
